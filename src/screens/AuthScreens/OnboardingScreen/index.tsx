b91e6607a4e634a2fab6a19c0405cf2303060081
Moizez <moizezhenrique@gmail.com>
2023-03-08 22:30:04 -0300

    - ajustes

--- src/screens/AuthScreens/OnboardingScreen/index.tsx
+++ src/screens/AuthScreens/OnboardingScreen/index.tsx
@@ -1,16 +1,13 @@
 import Onboarding from "react-native-onboarding-swiper";
 import { useDispatch } from "react-redux";
 import { onboardingPages } from "../../../constants/onboarding";
-import { useNavigation } from "@react-navigation/native";
-import { signInRequest, setOnboarding } from "../../../store/features/auth/auth-slice";
+import { signInRequest } from "../../../store/features/auth/auth-slice";
 
 const OnboardingScreen = () => {
   const dispatch = useDispatch();
-  const navigation = useNavigation();
 
   const handleFinishOnboarding = () => {
     dispatch(signInRequest());
-    navigation.navigate("SignInScreen");
   };
 
   return (
